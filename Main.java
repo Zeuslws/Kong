d8de8433190126b5815961eee4f99bc132ed9939
Zeuslws <118523955+Zeuslws@users.noreply.github.com>
2025-04-24 11:56:33 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ for(int j=0;j<numz.length;j++)
 {
 for(int i=0;i<numz.length-1;i++)
 {
-if(numz[i]<numz[i+1]){ // This should be > to sort ascending
+if(numz[i]>numz[i+1]){ // fixed: now it sorts ascending
 int swapTemp= numz[i]; 
 numz[i]=numz[i+1];
 numz[i+1]=swapTemp;
