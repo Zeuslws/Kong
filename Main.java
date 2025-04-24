1a10d8d99a24c7b8ff9ebf3dcb7ea1a1946fb5a9
Zeuslws <118523955+Zeuslws@users.noreply.github.com>
2025-04-24 11:45:46 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class zZZZZzzSortyMcSortFace {
+
+public static void sorterino(int[] numz){
+for(int j=0;j<numz.length;j++)
+{
+for(int i=0;i<numz.length-1;i++)
+{
+if(numz[i]<numz[i+1]){ // This should be > to sort ascending
+int swapTemp= numz[i]; 
+numz[i]=numz[i+1];
+numz[i+1]=swapTemp;
+}
+}
+} 
+System.out.println("done maybe?")
+}
+
+public static void main(String args[]){
+int mehArray[]={5,3,8,1,2};
+sorterino(mehArray);
+for(int p=0;p<mehArray.length;p++)System.out.print(mehArray[p]+" ");
+}
+}
